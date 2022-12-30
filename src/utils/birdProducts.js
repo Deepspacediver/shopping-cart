4267ebda2f5607a22113a2e54abcb4a75a93bd8d
deepspacediver <luka.koscik@gmail.com>
2022-12-30 22:07:41 +0100

    add products as objects

---
+++ src/utils/birdProducts.js
@@ -0,0 +1,68 @@
+import BirdOfParadise from "../assets/images/Bird-Of-Paradise.jpg";
+import HeavenlyCuckoo from "../assets/images/Heavenly-Cuckoo.jpg";
+import Lanius from "../assets/images/Lanius.jpg";
+import MangiapepeToucan from "../assets/images/Mangiapepe-Toucan.jpg";
+import RedMacaw from "../assets/images/Red-Macaw.jpg";
+import TucanaAracari from "../assets/images/Tucana-Aracari.jpg";
+import VariegatedParrot from "../assets/images/Variegated-Parrot.jpg";
+import YellowCrestedCockatoo from "../assets/images/Yellow-Crested-Cockatoo.jpg";
+import YellowHeadedParrot from "../assets/images/Yellow-Headed-Parrot.jpg";
+
+const birdProducts = [
+  {
+    id: "bird-of-paradise",
+    name: "Bird Of Paradise",
+    price: 130.0,
+    src: BirdOfParadise,
+  },
+  {
+    id: "heavenly-cuckoo",
+    name: "Heavenly Cuckoo",
+    price: 98.0,
+    src: HeavenlyCuckoo,
+  },
+  {
+    id: "lanius",
+    name: "Lanius",
+    price: 145.0,
+    src: Lanius,
+  },
+  {
+    id: "mangiapepe-toucan",
+    name: "Mangiapepe Toucan",
+    price: 215.0,
+    src: MangiapepeToucan,
+  },
+  {
+    id: "red-macaw",
+    name: "Red Macaw",
+    price: 118.0,
+    src: RedMacaw,
+  },
+  {
+    id: "tucana-aracari",
+    name: "TucanaAracari",
+    price: 198.0,
+    src: TucanaAracari,
+  },
+  {
+    id: "variegated-parrot",
+    name: "VariegatedParrot",
+    price: 149.0,
+    src: VariegatedParrot,
+  },
+  {
+    id: "yellow-crested-cockatoo",
+    name: "Yellow Crested Cockatoo",
+    price: 248.0,
+    src: YellowCrestedCockatoo,
+  },
+  {
+    id: "yellow-headed-parrot",
+    name: "Yellow Headed Parrot",
+    price: 151.0,
+    src: YellowHeadedParrot,
+  },
+];
+
+export default birdProducts;
