31569efb909e35e3711e4105237064dd60ad390e
deepspacediver <luka.koscik@gmail.com>
2022-12-31 17:24:18 +0100

    add Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import Navbar from "./Navbar/Navbar";
+
+const Header = () => (
+  <StyledHeader>
+    <h1>Header</h1>
+    <Navbar />
+  </StyledHeader>
+);
+
+const StyledHeader = styled.header``
+
+export default Header;
