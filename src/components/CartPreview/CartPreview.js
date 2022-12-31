db90ccc3c832182214d55f0ad88bcc14912166eb
deepspacediver <luka.koscik@gmail.com>
2022-12-31 16:51:11 +0100

    add styled wrapper

--- src/components/CartPreview/CartPreview.js
+++ src/components/CartPreview/CartPreview.js
@@ -1,21 +1,20 @@
 import { ShopCartContext } from "../../utils/ShopContext";
 import { ReactComponent as CartSVG } from "../../assets/icons/shopping-cart.svg";
 import { useContext } from "react";
+import styled from "styled-components";
 
 const CartPreview = () => {
   const shopCart = useContext(ShopCartContext);
   const getAmount = shopCart.reduce((total, curr) => total + curr.amount, 0);
   return (
     <>
-      <CartSVG
-        width="32px"
-        data-testid="svg"
-        role="img"
-        aria-labelledby="shopping-cart"
-      />
-      <p data-testid="cart-preview-display">{getAmount ? getAmount : ""}</p>
+      <CartSVG data-testid="svg" role="img" aria-labelledby="shopping-cart" />
+      <StyledParagraph data-testid="cart-preview-display">
+        {getAmount ? getAmount : ""}
+      </StyledParagraph>
     </>
   );
 };
 
+const StyledParagraph = styled.span``;
 export default CartPreview;
