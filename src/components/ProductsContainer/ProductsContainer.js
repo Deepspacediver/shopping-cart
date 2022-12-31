30a3293ff0a28db6152f51c0c7b7a9b77abb4bbb
deepspacediver <luka.koscik@gmail.com>
2022-12-31 14:08:09 +0100

    add ProductsContainer comp with tests

---
+++ src/components/ProductsContainer/ProductsContainer.js
@@ -0,0 +1,35 @@
+import birdProducts from "../../utils/birdProducts";
+import ShopProduct from "../Product/Product";
+
+import styled from "styled-components";
+
+const ProductsContainer = () => {
+  return (
+    <>
+      <h2>Bird Posters</h2>
+      <p data-testid="description">
+        Collection of Posters with Exotic Birds from all over the world
+      </p>
+      <ProductsWrapper>
+        {birdProducts.map((bird) => {
+          return (
+            <ShopProduct
+              key={bird.id}
+              id={bird.id}
+              name={bird.name}
+              price={bird.price}
+              src={bird.src}
+            />
+          );
+        })}
+      </ProductsWrapper>
+    </>
+  );
+};
+
+const ProductsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+`;
+
+export default ProductsContainer;
---
+++ src/components/ProductsContainer/ProductsContainer.test.js
@@ -0,0 +1,37 @@
+import ProductsContainer from "./ProductsContainer";
+import birdProducts from "../../utils/birdProducts";
+
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+describe("Products Container tests", () => {
+  test("if renders heading", () => {
+    render(<ProductsContainer />);
+
+    expect(
+      screen.getByRole("heading", { name: /Bird Posters/ })
+    ).toBeInTheDocument();
+  });
+
+  test("if renders description", () => {
+    render(<ProductsContainer />);
+
+    expect(
+      screen.getByTestId("description", {
+        name: /Collection of Posters with Exotic Birds from all over the world/,
+      })
+    ).toBeInTheDocument();
+  });
+
+  test("if renders products given an array of products", () => {
+    render(<ProductsContainer />);
+
+    expect(
+      screen.getByRole("heading", { name: /Red Macaw/ })
+    ).toBeInTheDocument();
+    expect(screen.getByRole("heading", { name: /Lanius/ })).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { name: /Bird Of Paradise/ })
+    ).toBeInTheDocument();
+  });
+});
