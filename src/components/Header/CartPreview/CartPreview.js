931fa97c0bd27d2177d42f39ccffc201b1ef5671
deepspacediver <luka.koscik@gmail.com>
2023-01-05 16:50:54 +0100

    add CartPreview styles

--- src/components/Header/CartPreview/CartPreview.js
+++ src/components/Header/CartPreview/CartPreview.js
@@ -1,20 +1,29 @@
 import { ShopCartContext } from "../../../utils/ShopContext";
 import { ReactComponent as CartSVG } from "../../../assets/icons/shopping-cart.svg";
 import { useContext } from "react";
-import styled from "styled-components";
+import "./CartPreview.css";
 
 const CartPreview = () => {
   const shopCart = useContext(ShopCartContext);
   const getAmount = shopCart.reduce((total, curr) => total + curr.amount, 0);
   return (
-    <>
-      <CartSVG data-testid="svg" role="img" aria-labelledby="shopping-cart" />
-      <StyledParagraph data-testid="cart-preview-display">
+    <figure className="header-nav_icon-wrapper">
+      <CartSVG
+        className="header-nav_cart-icon"
+        data-testid="svg"
+        role="img"
+        aria-labelledby="shopping-cart"
+      />
+      <figcaption
+        className={`header-nav_cart-amount${
+          getAmount ? "" : " header-nav_cart-amount--hidden"
+        }`}
+        data-testid="cart-preview-display"
+      >
         {getAmount ? getAmount : ""}
-      </StyledParagraph>
-    </>
+      </figcaption>
+    </figure>
   );
 };
 
-const StyledParagraph = styled.span``;
 export default CartPreview;
